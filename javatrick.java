dd926f381901eb8101f96431dea94343c81724c0
harshit <26hjain@gmail.com>
2019-03-08 18:50:42 +0530

    javatrick

---
+++ javatrick.java
@@ -0,0 +1,11 @@
+class javatrick
+{
+	public static void main(String args[])
+	{
+		//printing something without delimiter
+		//System.out.printf("hello hack"); without semicolon
+		if( System.out.printf("hello hack")==null)
+		{
+		}
+	}
+}
